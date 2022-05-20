fb25f538e61759cd9141b35d5b449f2e6e4726be
eung7 <wannabeeung@gmail.com>
2022-05-20 16:38:39 +0900

    Implementing Webservice Client

--- HotCoffee/Controllers/AddOrderViewController.swift
+++ HotCoffee/Controllers/AddOrderViewController.swift
@@ -10,4 +10,58 @@ import SnapKit
 
 class OrderViewController: UIViewController {
     
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        
+        return tableView
+    }()
+    
+    var coffeeSize: UISegmentedControl = {
+        let control = UISegmentedControl()
+        control.insertSegment(withTitle: "Small", at: 0, animated: false)
+        control.insertSegment(withTitle: "Medium", at: 1, animated: false)
+        control.insertSegment(withTitle: "Large", at: 2, animated: false)
+        
+        return control
+    }()
+    
+    var nameTextField: UITextField = {
+        let textField = UITextField()
+        
+        return textField
+    }()
+    
+    var emailTextField: UITextField = {
+        let textField = UITextField()
+        
+        return textField
+    }()
+    
+    var closeBarButtonItem: UIBarButtonItem = {
+        let barButtonItem = UIBarButtonItem()
+        barButtonItem.title = "Close"
+        
+        return barButtonItem
+    }()
+    
+    var saveBarButtonItem: UIBarButtonItem = {
+        let barButtonItem = UIBarButtonItem()
+        barButtonItem.title = "Save"
+        
+        return barButtonItem
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        title = "Add New Order"
+        
+        tableView.snp.makeConstraints { make in
+            make.leading.trailing.top.equalToSuperview()
+            make.height.equalTo(UIScreen.main.bounds.height / 2)
+        }
+    }
 }
